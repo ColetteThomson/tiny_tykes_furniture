6b256b463f88fab1937ec76d3b34f92f1916d271
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-09-26 10:35:10 +0000

    add menu_display function

--- run.py
+++ run.py
@@ -14,5 +14,33 @@ SHEET = GSPREAD_CLIENT.open('tiny_tykes_furniture')
 
 inventory = SHEET.worksheet('inventory')
 
-data = inventory.get_all_values()
-print(data)
+#data = inventory.get_all_values()
+#print(data)
+
+def menu_display():
+    print('Welcome to Tiny Tykes Furniture Inventory')
+    print('(1) Update Inventory Item')
+    print('(2) Search Inventory')
+    print('(3) Add New Inventory Item')
+    print('(4) Delete Inventory Item')
+    print('(5) Print Inventory Report')
+    print('(6) Quit')
+
+    OPTION = int(input('Enter number choice: '))
+    menu_option(OPTION)
+
+def menu_option(OPTION):
+    if OPTION == 1:
+        update_inventory()
+    elif OPTION == 2:
+        search_inventory()
+    elif OPTION == 3:
+        add_inventory()
+    elif OPTION == 4:
+        delete_inventory()
+    elif OPTION == 5:
+        print_inventory()
+    elif OPTION == 6:
+        exit()
+
+menu_display()

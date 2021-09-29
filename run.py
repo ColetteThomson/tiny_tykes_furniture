76cf8569aaa5ee394e808092d55b38603ebadadd
ColetteThomson <colette_f_thomson@yahoo.co.uk>
2021-09-29 11:58:38 +0000

    fix wording and naming; add input_date

--- run.py
+++ run.py
@@ -13,9 +13,9 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('tiny_tykes_furniture')
 
 
-class hotelmanage:
-    def __init__(self, rt='', s=0, p=0, r=0, t=0, a=1000, name='', address='', cindate='', coutdate='', rno=1):
-        print("\n\n*****WELCOME TO THE HOTEL CALIFORNIA*****\n")
+class hotel_manage:
+    def __init__(self, rt='', s=0, p=0, r=0, t=0, a=1000, name='', address='', checkin_date='', coutdate='', rno=1):
+        print("\n*****WELCOME TO THE HOTEL CALIFORNIA*****\n")
         self.rt = rt
         self.r = r
         self.t = t
@@ -23,140 +23,107 @@ class hotelmanage:
         self.s = s
         self.a = a
         self.name = name
-        self.address = address
-        self.cindate = cindate
-        self.coutdate = coutdate
+        #self.address = address
+        self.checkin_date = checkin_date
+        #self.coutdate = coutdate
         self.rno = rno
-    """
-    def validate_string(values):
-        try:
-            if not isalpha():
-                raise ValueError(
-                    f"Please use letters of the alphabet, you entered{e}\n"
-                )
-        except ValueError as e:
-                print(f"Invalid data: {e}, please try again.\n")
-                return False
-        return True """
-
-    def inputdata(self):
+    
+    def input_name(self):
         while True:
-            #try:
-            self.name = input('\nEnter your full name:')
-            if self.name.isalpha():
-                print('Data is valid')
-                    # return self.name
-            break
-        else:
-            print('Please enter letters only.')
-                    #raise TypeError
-            #except TypeError:
-                #print('Please enter letters only.')
-                #eturn False
+            try:
+                self.name = input('\nEnter your surname:')
+                if self.name.isalpha():
+                    print('Data is valid')
+                    #return self.name
+                    break
+                else:
+                    raise TypeError
+            except TypeError:
+                print('Please select "1" and re-enter with letters only.')
+                main()
+                #return False
                             
-            
-            self.address = input("\nEnter your location (city):")
-            self.cindate = input("\nEnter your check in date:")
-            self.coutdate = input("\nEnter your checkout date:")
-        
-        print("Your room no.:", self.rno, "\n")
-    """
-    def validate_string(values):
-        try:
-            if not isalpha():
-                raise ValueError(
-                    f"Please use letters of the alphabet, you entered{e}\n"
-                )
-        except ValueError as e:
-                print(f"Invalid data: {e}, please try again.\n")
-                return False
-        return True
-        """
+    def input_date(self):
+        self.checkin_date = input("\nEnter your check in date: \n")
+           #print("Your room no.:", self.rno, "\n")
+    
 
-    def roomrent(self):
-        print("We have the following rooms for you:-")
-        print("1.  Class A----&gt;4000")
-        print("2.  Class B----&gt;3000")
-        print("3.  Class C----&gt;2000")
-        print("4.  Class D----&gt;1000")
+    def room_rent(self):
+        print('We have the following room types available:\n')
+        print('1.  Family : £100 pn')
+        print('2.  Twin Bed : £80 pps')
+        print('3.  Double : £70 pps')
+        print('4.  Single : £60 pp')
 
-        x = int(input("Enter the number of your choice Please-&gt;"))
-        n = int(input("For How Many Nights Did You Stay:"))
+        x = int(input('\nPlease enter the Number of your choice (example: 1): \n'))
+        n = int(input('Please enter Number of nights you would like to stay with us? (example: 2): \n'))
 
         if (x == 1):
-            print("you have choose room Class A")
-            self.s = 4000 * n
+            print('You have selected a Family room\n')
+            self.s = 100 * n
 
         elif (x == 2):
-            print("you have choose room Class B")
-            self.s = 3000 * n
+            print('You have selected a Twin Bed room\n')
+            self.s = 80 * n
 
         elif (x == 3):
-            print("you have choose room Class C")
-            self.s = 2000 * n
+            print('You have selected a Double room\n')
+            self.s = 70 * n
 
         elif (x == 4):
-            print("you have choose room Class D")
-            self.s = 1000 * n
+            print('You have selected a Single room\n')
+            self.s = 60 * n
 
         else:
-            print("please choose a room")
-            print("your choosen room rent is =", self.s, "\n")
+            print('Please select a room type')
+            print('Your chosen room rent is =", self.s, "\n')
 
-    def foodpurchased(self):
+    def food_purchased(self):
         print("*****RESTAURANT MENU*****")
-        print("1.Dessert-----&gt;100", "2.Drinks-----&gt;50", "3.Breakfast---&gt;90", "4.Lunch----&gt;110", "5.Dinner---&gt;150", "6.Exit")
+        print('1. Dinner : £40 pp\n', '2. Breakfast : £15 pp\n', '3. Lunch : £30 pp\n', '4. Exit from Menu\n')
 
         while (1):
-            c = int(input("Enter the number of your choice:"))
+            c = int(input('Enter the Menu number of your choice.: \n'))
 
             if (c == 1):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 100 * d
+                d = int(input('For how many people (example: 2): \n'))
+                self.r = self.r + 40 * d
 
             elif (c == 2):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 50 * d
+                d = int(input('For how many people (example: 2): \n'))
+                self.r = self.r + 15 * d
 
             elif (c == 3):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 90 * d
+                d = int(input('For how many people (example: 2): \n'))
+                self.r = self.r + 30 * d
 
             elif (c == 4):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 110 * d
-
-            elif (c == 5):
-                d = int(input("Enter the quantity:"))
-                self.r = self.r + 150 * d
-
-            elif (c == 6):
                 break
 
             else:
                 print("You've Enter an Invalid Key")
                 print("Total food Cost=Rs", self.r, "\n")
 
-    def display(self):
-        print("******HOTEL BILL******")
-        print("Customer details:")
-        print("Customer name:", self.name)
-        print("Customer address:", self.address)
-        print("Check in date:", self.cindate)
-        print("Check out date", self.coutdate)
-        print("Room no.", self.rno)
-        print("Your Room rent is:", self.s)
-        print("Your Food bill is:", self.r)
+    def show_final_bill(self):
+        print('******HOTEL CALIFORNIA BILL******')
+        print('Customer details: ')
+        print('Customer name:', self.name)
+        #print("Customer address:", self.address)
+        print('Check in date:', self.checkin_date)
+        #print("Check out date", self.coutdate)
+        print('Room no.', self.rno)
+        print('Your Room rent is:', self.s)
+        print('Your Food bill is:', self.r)
         self.rt = self.s + self.t + self.p + self.r
 
-        print("Your sub total Purchased is:", self.rt)
-        print("Additional Service Charges is", self.a)
-        print("Your grandtotal Purchased is:", self.rt + self.a, "\n")
+        #print('Your sub total Purchased is:', self.rt)
+        #print('Additional Service Charges is', self.a)
+        print('Your Final Bill cost is:', self.rt + self.a, '\n')
         self.rno += 1
 
 
 def main():
-    a = hotelmanage()
+    a = hotel_manage()
 
     while (1):
         print("1.Enter Customer Data")
@@ -168,19 +135,32 @@ def main():
         b = int(input("\nEnter the number of your choice:"))
 
         if (b == 1):
-            a.inputdata()
+            a.input_name()
+            a.input_date()
 
         if (b == 2):
-            a.roomrent()
+            a.room_rent()
 
         if (b == 3):
-            a.foodpurchased()
+            a.food_purchased()
 
         if (b == 4):
-            a.display()
+            a.show_final_bill()
 
         if (b == 5):
             quit()
 
 
 main()
+
+"""
+    def validate_string(values):
+        try:
+            if not isalpha():
+                raise ValueError(
+                    f"Please use letters of the alphabet, you entered{e}\n"
+                )
+        except ValueError as e:
+                print(f"Invalid data: {e}, please try again.\n")
+                return False
+        return True """
